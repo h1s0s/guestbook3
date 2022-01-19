1416199e8084a8cad4aafed865e98d676e8abe91
한상선 <hs94122@naver.com>
2022-01-19 16:05:39 +0900

    2022-01-19 16:05

--- src/main/java/com/javaex/dao/GuestbookDao.java
+++ src/main/java/com/javaex/dao/GuestbookDao.java
@@ -8,6 +8,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import com.javaex.vo.GuestbookVo;
 
 public class GuestbookDao {
